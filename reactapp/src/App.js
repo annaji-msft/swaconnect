ecdf287931354ad41e16dd4794be66fa14fd64e0
Annaji Sharma Ganti <anganti@microsoft.com>
2021-08-20 13:13:55 -0700

    fresh

---
+++ reactapp/src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function App() {
+  const value = 'World';
+  return <div>Hello {value}<a className="App-link" href="/login"> Login with Google </a><a className="App-link" href="/logout"> Logout</a> </div>;
+}
+
+export default App;
