08156de659aea5c668f3d988dd038da368c4e30e
Annaji Sharma Ganti <anganti@microsoft.com>
2021-08-20 10:34:47 -0700

    add state

--- reactapp/src/App.tsx
+++ reactapp/src/App.tsx
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect }from 'react';
 import './App.css';
-import { Login } from '@microsoft/mgt-react';
+import { Login, Person } from '@microsoft/mgt-react';
+import { Providers, ProviderState } from '@microsoft/mgt-element';
+
+function useIsSignedIn(): [boolean] {
+  const [isSignedIn, setIsSignedIn] = useState(false);
+
+  useEffect(() => {
+    const updateState = () => {
+      const provider = Providers.globalProvider;
+      setIsSignedIn(provider && provider.state === ProviderState.SignedIn);
+    };
+
+    Providers.onProviderUpdated(updateState);
+    updateState();
+
+    return () => {
+      Providers.removeProviderUpdatedListener(updateState);
+    }
+  }, []);
+
+  return [isSignedIn];
+}
 
 function App() {
+  const [isSignedIn] = useIsSignedIn();
+
   return (
     <div className="App">
       <header>
@@ -10,6 +33,10 @@ function App() {
         <a className="App-link" href="/login"> Login with Google </a>
         <a className="App-link" href="/logout"> Logout</a>
       </header>
+      <div>
+        {isSignedIn &&
+          <Person personQuery="me" />}
+      </div>
     </div>
   );
 }
