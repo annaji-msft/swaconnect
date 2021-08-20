b9aa0a3a121f4ce09eb9331307e067395f0a73aa
Annaji Sharma Ganti <anganti@microsoft.com>
2021-08-19 23:52:55 -0700

    fix

--- app/src/index.js
+++ app/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {Providers, ProxyProvider} from '@microsoft/mgt';
+import {Providers, ProxyProvider} from '@microsoft/mgt-react';
 
 let provider = new ProxyProvider("/api/proxy", async () => {
   return {
