b0104ed10b8a5f5d0120339f2d83d04e1d8af1b2
Annaji Sharma Ganti <anganti@microsoft.com>
2021-08-20 16:05:10 -0700

    graph

--- app/src/App.js
+++ app/src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import axios from 'axios';
 import { DefaultButton, ThemeProvider, initializeIcons, Stack, Label } from '@fluentui/react';
 
+import Graph from './Graph'
+
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      score: 0,
-      userDetails: undefined,
-      connectedToUserTwitter: false
+      userId: undefined,
+      identityProvider: undefined
     };
     initializeIcons();
   }
@@ -17,7 +18,7 @@ class App extends React.Component {
     axios.post('/.auth/me')
       .then(userInfo => {
         if(userInfo && userInfo.data.clientPrincipal != null) {
-          this.setState({ userDetails: userInfo })
+          this.setState({ userId: userInfo.data.clientPrincipal.userDetails, identityProvider: userInfo.data.clientPrincipal.identityProvider })
          }
       })
   }
@@ -31,23 +32,27 @@ class App extends React.Component {
                padding: 10,
              }}>
                <Label>To get started, please login with one of the authentication providers!</Label>
-               <DefaultButton iconProps={{ iconName: 'AuthenticatorApp' }} href="/.auth/login/aad" disabled={this.state.userDetails !== undefined} >
+               <DefaultButton iconProps={{ iconName: 'AuthenticatorApp' }} href="/.auth/login/aad" disabled={this.state.userId !== undefined} >
                  Login with Microsoft
                </DefaultButton>
-               <DefaultButton iconProps={{ iconName: 'AuthenticatorApp' }} href="/.auth/login/twitter" disabled={this.state.userDetails !== undefined} >
+               <DefaultButton iconProps={{ iconName: 'AuthenticatorApp' }} href="/.auth/login/twitter" disabled={this.state.userId !== undefined} >
                  Login with Twitter
                </DefaultButton>
-               <DefaultButton iconProps={{ iconName: 'AuthenticatorApp' }} href="/.auth/login/github" disabled={this.state.userDetails !== undefined} >
+               <DefaultButton iconProps={{ iconName: 'AuthenticatorApp' }} href="/.auth/login/github" disabled={this.state.userId !== undefined} >
                  Login with Github
                </DefaultButton>
-               <DefaultButton iconProps={{ iconName: 'AuthenticatorApp' }} href="/.auth/login/google" disabled={this.state.userDetails !== undefined} >
+               <DefaultButton iconProps={{ iconName: 'AuthenticatorApp' }} href="/.auth/login/google" disabled={this.state.userId !== undefined} >
                  Login with Google
                </DefaultButton>
-               <DefaultButton href="/.auth/logout" disabled={this.state.userDetails === undefined}>
+               <DefaultButton href="/.auth/logout" disabled={this.state.userId === undefined}>
                  Logout
              </DefaultButton>
              </Stack>
-             <Label>Hello, "{this.state.userDetails}". You are logged in using "{this.state.identityProvider}"  Let's connect to some services ...</Label>
+             <Label>Hello, "{this.state.userId}". You are logged in using "{this.state.identityProvider}"  Let's connect to some services ...</Label>
+             <br />
+             <br />
+             {this.state.userId !== undefined 
+             && <Graph />}
        </ThemeProvider>
        </header>
        </div>);
---
+++ app/src/Graph.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import axios from 'axios';
+import { DefaultButton, ThemeProvider, Stack, Label } from '@fluentui/react';
+
+class Graph extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      connected: false,
+      token: ""
+    };
+  }
+
+  componentDidMount() {
+    axios.get('/api/.token/status/graph')
+      .then(response => {
+        if(response && response == 'connected') {
+          this.setState({ connected: true })
+         }
+      });
+  }
+
+  create = () => {
+    axios.post("/api/.token/create/graph")
+    .then(() => {
+      this.setState({ connected: true });
+    }).catch((error) => {
+      if (error.response
+        && error.response.status === 401
+        && error.response) {
+        // Initiate consent flow with redirect url.
+        window.location.href = error.response;
+      } else {
+        alert(error);
+      }
+    });
+  }
+
+  delete = () => {
+    axios.post("/api/.token/delete/graph")
+      .then(() => {
+        this.setState({ connected: false });
+      }).catch((error) => {
+        alert(error);
+      });
+  }
+
+  getToken = () => {
+    axios.get("/api/.token/graph")
+    .then((token) => {
+      this.setState({ token: token });
+    }).catch((error) => {
+      alert(error);
+    });
+  }
+
+  render() { 
+    return (<div>
+      <ThemeProvider>
+      <Label>Graph Token</Label>
+         <Stack verticalAlign tokens={{
+               childrenGap: 5,
+               padding: 5,
+               maxWidth: "50px"
+             }}>
+
+               <DefaultButton iconProps={{ iconName: 'PlugConnected' }} onClick={this.create} disabled={this.state.connected == true} >
+                 Create
+               </DefaultButton>
+
+               <DefaultButton iconProps={{ iconName: 'PlugDisconnected' }} onClick={this.delete}  disabled={this.state.connected == false}>
+                 Delete
+               </DefaultButton>
+
+               <DefaultButton iconProps={{ iconName: 'AzureKeyVault' }} onClick={this.getToken}  disabled={this.state.connected == false} >
+                 Display
+               </DefaultButton>
+
+             </Stack>
+
+             <Label>{this.state.token}</Label>
+       </ThemeProvider>
+       </div>);
+    }
+  }
+
+export default Graph;
