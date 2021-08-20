3d83c44298c969be3b469dbaabb18cc9e6a4bf34
Annaji Sharma Ganti <anganti@microsoft.com>
2021-08-20 13:48:06 -0700

    f

--- app/src/index.js
+++ app/src/index.js
@@ -2,14 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { Providers } from '@microsoft/mgt-element';
-import { ProxyProvider } from '@microsoft/mgt-proxy-provider';
-
-Providers.globalProvider = new ProxyProvider("/api/proxy", async () => {
-    return {
-      "X-MS-TOKENPROVIDER-ID": 'graph',
-      "X-MS-PROXY-BACKEND-HOST": 'https://graph.microsoft.com'
-    };
-  });
 
 ReactDOM.render(<App />, document.getElementById('root'));
