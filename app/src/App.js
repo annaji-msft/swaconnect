a0c2a3c2cfdb6efc239d58dd5e5ef85b5bcec0f1
Annaji Sharma Ganti <anganti@microsoft.com>
2021-08-20 14:14:34 -0700

    fluent

--- app/src/App.js
+++ app/src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import { DefaultButton, ThemeProvider, initializeIcons } from '@fluentui/react';
+
+initializeIcons();
 
 function App() {
   const value = 'World';
-  return <div>Hello {value}<a className="App-link" href="/login"> Login with Google </a><a className="App-link" href="/logout"> Logout</a></div>;
+  return (<div>Hello {value}<a className="App-link" href="/login"> Login with Google </a><a className="App-link" href="/logout"> Logout</a>
+   <ThemeProvider>
+      <DefaultButton onClick={() => alert('hello')}>Hello World</DefaultButton>
+    </ThemeProvider></div>);
 }
 
 export default App;
